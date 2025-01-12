48d2987a7a4923c270c12bc130d2854b35760e64
00hung <00huang.870929@gmail.com>
2025-01-12 16:40:43 +0800

    facter:新增地圖

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,7 @@
+/* ----------------------------------- */
+/* ------ Plugin ------ */
+/* ----------------------------------- */
+
 // Initialize Lenis
 const lenis = new Lenis({
   autoRaf: true,
@@ -8,14 +12,14 @@ const lenis = new Lenis({
 //   console.log(e);
 // });
 
-// const $lazyImgs = document.querySelector('img.js-lazy');
-// $lazyImgs.forEach(function(el) {
+const $lazyImgs = document.querySelectorAll('img.js-lazy');
+$lazyImgs.forEach(function(el) {
 //   // https://png-pixel.com/
-//   el.setAttribute(
-//     'src',
-//     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAQAAAAe/WZNAAAADklEQVR42mNkgAJGDAYAAFEABCaLYqoAAAAASUVORK5CYII='
-//   );
-// });
+   el.setAttribute(
+    'src',
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAQAAAAe/WZNAAAADklEQVR42mNkgAJGDAYAAFEABCaLYqoAAAAASUVORK5CYII='
+  );
+ });
 
 const lazyLoad = new LazyLoad({
   elements_selector: '.js-lazy',
@@ -25,5 +29,70 @@ const lazyLoad = new LazyLoad({
     // 避免圖片載入之後，aos 進場動畫偵測跑掉
     // 因為有可能圖片載入前載入後的佔位不同高
     AOS.refresh();
+  }
+});
+
+
+/* ----------------------------------- */
+/* ------ Custom ------ */
+/* ----------------------------------- */
+const pathname = location.pathname;
+const hash = location.hash;
+const path = pathname + hash;
+console.log(pathname);
+const $a = document.querySelectorAll('a');
+$a.forEach(function(el){
+  const href = el.getAttribute('href');
+  if (path.includes(href)) {
+    el.classList.add('is-active');
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+const $nav = document.querySelector(`.c-nav`);
+const $burger = document.querySelector('.o-burger');
+let isNavOpened = false;
+
+function toggleNav() {
+  if(!isNavOpened){
+    lenis.stop();
+    $nav.classList.add('is-opened');
+    $burger.classList.add('is-opened');
+    isNavOpened = true;
+  }else {
+    lenis.start();
+    $nav.classList.remove('is-opened');
+    $burger.classList.remove('is-opened');
+    isNavOpened = false;
+  }
+}
+
+window.matchMedia('(min-width: 1400px)').addEventListener('change', function(event) {
+  // isNavOpened true 才有執行動作的必要
+  if (isNavOpened) {
+    if (event.matches) lenis.start();
+    else lenis.stop();
+  }
+})
+
+const $homeNews = document.querySelector('.l-home-news');
+const $body = document.body;
+window.addEventListener('scroll', function() {
+  
+  const newsTop = $homeNews.getBoundingClientRect().top;
+  const end = newsTop + $homeNews.offsetHeight / 2
+  if (newsTop <= 100 && end >= 0) {
+    $body.classList.add('is-news-active');
+  } else {
+    $body.classList.remove('is-news-active');
   }
 });
