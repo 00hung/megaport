f586562315e63477ed0dff47c545985cde3767d5
00hung <00huang.870929@gmail.com>
2025-01-12 14:26:28 +0800

    新增 : lazy load

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,29 @@
 // Initialize Lenis
 const lenis = new Lenis({
-    autoRaf: true,
-  });
-  
-  // Listen for the scroll event and log the event data
-  // lenis.on('scroll', (e) => {
-  //   console.log(e);
-  // });
-  
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+// lenis.on('scroll', (e) => {
+//   console.log(e);
+// });
+
+// const $lazyImgs = document.querySelector('img.js-lazy');
+// $lazyImgs.forEach(function(el) {
+//   // https://png-pixel.com/
+//   el.setAttribute(
+//     'src',
+//     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAQAAAAe/WZNAAAADklEQVR42mNkgAJGDAYAAFEABCaLYqoAAAAASUVORK5CYII='
+//   );
+// });
+
+const lazyLoad = new LazyLoad({
+  elements_selector: '.js-lazy',
+   // 設定距離可視區(視窗)底部多遠觸發
+  threshold: 500,
+  callback_loaded: function() {
+    // 避免圖片載入之後，aos 進場動畫偵測跑掉
+    // 因為有可能圖片載入前載入後的佔位不同高
+    AOS.refresh();
+  }
+});
